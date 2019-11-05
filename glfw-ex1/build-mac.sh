014630f97507fb09054457665f6fb804728e3d81
Tamas Losonczi <losonczi.tamas91@gmail.com>
2019-11-05 16:40:30 +0100

    [glfw-ex1] fix VBO order issue

--- glfw-ex1/build-mac.sh
+++ glfw-ex1/build-mac.sh
@@ -4,4 +4,4 @@ INCLUDES="-I../include"
 LINKER_FLAGS="-L../libs -lglfw3 -framework OpenGL -framework Cocoa -framework IOkit -framework CoreVideo"
 SOURCES="*.c ../src/*/*.c"
 
-clang $SOURCES $INCLUDES $LINKER_FLAGS -g -o main.out
+clang $SOURCES $INCLUDES $LINKER_FLAGS -Wall -g -o main.out
--- glfw-ex1/main.c
+++ glfw-ex1/main.c
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include "utils/utils.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 // Try to draw 2 triangles next to each other using glDrawArrays by adding more vertices to your data.
 
@@ -29,7 +30,7 @@ EBO CreateEBO(unsigned int *indices, unsigned int indexCount)
     EBO result;
     glGenBuffers(1, &result);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, result);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * indexCount, indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexCount * sizeof(unsigned int), indices, GL_STATIC_DRAW);
     return result;
 }
 
@@ -67,6 +68,15 @@ Shader CreateShaderProgram(Shader vertex, Shader fragment)
     return program;
 }
 
+void _SafeFree(void* memory)
+{
+    if (memory)
+    {
+        free(memory);
+    }
+}
+#define SafeFree(memory) _SafeFree((void*) (memory));
+
 int main()
 {
     if (!glfwInit())
@@ -86,28 +96,31 @@ int main()
             //TODO: switch to makefiles
             const char* vertexShaderSource = Utils_ReadTextFile("ex1.vert");
             const char* fragmentShaderSource = Utils_ReadTextFile("ex1.frag");
-            //TODO: use index buffer
+
             if (fragmentShaderSource && vertexShaderSource)
             {
                 float vertices[] =
                 {
                        -1.0f, -0.5f, 0.0f, 
-                        0.0f, -0.5f, 0.0f, 
-                       -0.5,   0.5f, 0.0f, 
-                        1.0f, -0.5f, 0.0f 
+                        0.0f, -0.5f, 0.0f,    
+                       -0.5,   0.5f, 0.0f,  
+                        1.0f, -0.5f, 0.0f,  
+                        0.5f,  0.5f, 0.0f
                 };
 
                 unsigned int indices[] =
                 {
                     0, 1, 2,
-                    1, 3, 2
+                    1, 3, 4
                 };
 
-                EBO ebo = CreateEBO(indices, ArraySize(indices));
                 VBO vbo = CreateVBO(vertices, ArraySize(vertices));
+
                 VAO vao = CreateVAO();
                 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
                 glEnableVertexAttribArray(0);
+
+                EBO ebo = CreateEBO(indices, ArraySize(indices));
 
                 Shader vertex = CreateShader(GL_VERTEX_SHADER, vertexShaderSource);
                 Utils_CheckShaderState(vertex, window);
@@ -115,7 +128,6 @@ int main()
                 Utils_CheckShaderState(fragment, window);
                 Program program = CreateShaderProgram(vertex, fragment);
                 Utils_CheckProgramState(program, window);
-
                 glUseProgram(program);
 
                 while (!glfwWindowShouldClose(window))
@@ -123,16 +135,19 @@ int main()
                     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
                     glClear(GL_COLOR_BUFFER_BIT);
                     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
                     glfwSwapBuffers(window);
                     glfwPollEvents();
                 }
 
+                glDeleteBuffers(1, &ebo);
                 glDeleteBuffers(1, &vbo);
                 glDeleteVertexArrays(1, &vao);
                 glDeleteProgram(program);
                 glDeleteShader(vertex);
                 glDeleteShader(fragment);
+
+                SafeFree(vertexShaderSource);
+                SafeFree(fragmentShaderSource);
             }
             glfwDestroyWindow(window);
         }
