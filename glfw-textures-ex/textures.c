1fa90130535bd60848f9d7befece0cf3a256793c
LosoncziTamas <losonczi.tamas91@gmail.com>
2019-11-12 07:39:59 +0100

    [glfw-textures] texture loading & display WIP

--- glfw-textures-ex/textures.c
+++ glfw-textures-ex/textures.c
@@ -1,6 +1,8 @@
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 #include "utils/utils.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "utils/stb_image.h"
 
 #include <assert.h>
 
@@ -17,6 +19,8 @@ void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods
     }
 }
 
+// ex-1: Make sure only the happy face looks in the other/reverse direction by changing the fragment shader.
+
 int main()
 {
     assert(glfwInit());
@@ -26,6 +30,12 @@ int main()
 
     glfwSetFramebufferSizeCallback(window, FrameBufferSizeCallback);
     glfwSetKeyCallback(window, KeyCallback);
+
+    const char* vertSrc = Utils_ReadTextFile("texture.vert");
+    const char* fragSrc = Utils_ReadTextFile("texture.frag");
+    
+    int w, h, channelCount;
+    unsigned char imageData = stbi_load("busojaras.png", &w, &h, &channelCount, 0);
 
     while (!glfwWindowShouldClose(window))
     {
